e6b9f8037d17bd39b3f392ca8daf08215c222da5
Andre Silva <and.silva416@gmail.com>
2021-01-14 23:39:35 +0000

    not done

--- src/movies.js
+++ src/movies.js
@@ -1,15 +1,54 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+function getAllDirectors(moviesArr) {
+  let directorArr = moviesArr.map((movie => {
+    return movie.director  
+  }))
+  return directorArr
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(moviesArr) {
+    let mappedArr = movies.filter(function(elem) {
+      
+    })
+}
+
+
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+
+function ratesAverage(moviesArr) {
+
+}
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
+function dramaMoviesRtae(moviesArr) {
+  const filterDrama = arr.filter( )
+}
+
+
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(moviesArr) {
+
+}
+
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+function turnHoursToMinutes(moviesarr) {
+
+}
+
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
